136eb420bda2ae4b518ecf732a85568761b0cf09
Your Name <you@example.com>
2026-04-21 07:01:37 -0500

    first commit

--- src/components/view/Condiments.tsx
+++ src/components/view/Condiments.tsx
@@ -34,7 +34,7 @@ export default function Condiments() {
   return (
     <div>
 <h1 className="text-3xl font-bold text-center mb-8"> Condiments</h1>
-    <div className="container mx-auto py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8 ">
+    <div className="container mx-auto py-8 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 gap-6">
       {images.map((image, index) => (
        <Link key={index} to={`/produit/${image.id}`}>
         <div >
